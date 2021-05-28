25105e0b402c9c4889abd9cb4b79ac4f4a24a7b1
SirBob01 <kleonardo52@gmail.com>
2021-05-28 23:27:01 +1000

    Implement neural network subengine

---
+++ engine/neural/network.h
@@ -0,0 +1,70 @@
+#ifndef CHESS_NEURAL_NETWORK_H_
+#define CHESS_NEURAL_NETWORK_H_
+
+#include <vector>
+#include <random>
+
+#include "matrix.h"
+
+namespace chess::neural {
+    // Activation/cost functions must take a double and a boolean indicating
+    // whether or not it's the derivative function
+    using activation = double (*)(double x, bool derivative);
+    using cost = double (*)(double predicted_y, double expected_y, bool derivative);
+
+    struct Layer {
+        int nodes;
+        activation activation_function;
+    };
+
+    struct NetworkParameters {
+        cost cost_function;
+        double learning_rate;
+        double gradient_clip;
+        std::vector<Layer> layers;
+    };
+
+    class Network {
+        std::vector<Matrix> _z;
+        std::vector<Matrix> _a;
+        std::vector<Matrix> _biases;
+        std::vector<Matrix> _weights;
+
+        std::vector<activation> _activations;
+        cost _cost;
+
+        double _learning_rate;
+
+        /**
+         * Apply the cost function to matrices
+         */
+        Matrix apply_cost(Matrix &predicted, Matrix &expected, bool derivative);
+
+        /**
+         * Feed-forward algorithm
+         */
+        Matrix forward(Matrix input);
+
+        /**
+         * Backpropagation gradient descent algorithm
+         */
+        void backward(Matrix expected);
+
+    public:
+        Network(NetworkParameters params);
+
+        /**
+         * Train network on some data
+         */
+        void fit(std::vector<double> input, std::vector<double> output);
+
+        /**
+         * Evaluate the network on some input data
+         */
+        inline Matrix evaluate(std::vector<double> input) {
+            return forward({1, _weights[0].get_rows(), input});
+        }
+    };
+}
+
+#endif
