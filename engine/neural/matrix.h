c42b7ce1b928bbb0e9442b4a11e4169f71278ee5
SirBob01 <kleonardo52@gmail.com>
2021-05-28 23:27:01 +1000

    Implement neural network subengine

---
+++ engine/neural/matrix.h
@@ -0,0 +1,140 @@
+#ifndef CHESS_NEURAL_MATRIX_H_
+#define CHESS_NEURAL_MATRIX_H_
+
+#include <vector>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+
+namespace chess::neural {
+    class Matrix {
+        double *_data;
+        int _rows;
+        int _cols;
+
+    public:
+        Matrix(int rows, int cols);
+        Matrix(int rows, int cols, double val);
+        Matrix(int rows, int cols, double data[]);
+        Matrix(int rows, int cols, std::vector<double> data);
+        Matrix(std::vector<std::vector<double>> data);
+        Matrix(const Matrix &rhs);
+        ~Matrix();
+
+        /**
+         * Assignment operator
+         */
+        Matrix &operator=(const Matrix &rhs);
+
+        /**
+         * Addition operator
+         */
+        const Matrix operator+(const Matrix &rhs) const;
+
+        /**
+         * Subtraction operator
+         */
+        const Matrix operator-(const Matrix &rhs) const;
+
+        /**
+         * Matrix multiplication
+         */
+        const Matrix operator*(const Matrix &rhs) const;
+
+        /**
+         * Scale operator
+         */
+        const Matrix operator*(const double factor) const;
+
+        /**
+         * Division operator
+         */
+        const Matrix operator/(const double factor) const;
+
+        /**
+         * Hadamard operator
+         */
+        const Matrix operator^(const Matrix &rhs) const;
+
+        /**
+         * In-place addition operator
+         */
+        void operator+=(const Matrix &rhs);
+
+        /**
+         * In-place subtraction operator
+         */
+        void operator-=(const Matrix &rhs);
+
+        /**
+         * In-place matrix multiplication
+         */
+        void operator*=(const Matrix &rhs);
+
+        /**
+         * In-place scale operator
+         */
+        void operator*=(const double factor);
+
+        /**
+         * In-place division operator
+         */
+        void operator/=(const double factor);
+
+        /**
+         * In-place hadamard operator
+         */
+        void operator^=(const Matrix &rhs);
+
+        /**
+         * Call a function on each item of the matrix
+         */
+        void map(std::function<double(double&)> function);
+
+        /**
+         * Get the transpose of the matrix
+         */
+        Matrix transpose() const;
+
+        /**
+         * Make all values zero
+         */
+        void zero();
+
+        /**
+         * Fetch a value from the matrix
+         */
+        inline double get_at(const int row, const int col) const {
+            return _data[row * _cols + col];
+        }
+
+        /**
+         * Set a value in the matrix
+         */
+        inline void set_at(const int row, const int col, const double value) {
+            _data[row * _cols + col] = value;
+        }
+
+        /**
+         * Get the number of rows in the matrix;
+         */
+        inline int get_rows() const {
+            return _rows;
+        }
+
+        /**
+         * Get the number of columns in the matrix
+         */
+        inline int get_cols() const {
+            return _cols;
+        }
+
+        /**
+         * Pretty print the matrix for debugging
+         */
+        void print() const;
+    };
+}
+
+#endif
---
+++ engine/neural/network.cpp
@@ -0,0 +1,82 @@
+#include "network.h"
+
+namespace chess::neural {
+    Network::Network(NetworkParameters params) {
+        int n = params.layers.size();
+
+        std::random_device rd;
+        std::mt19937 generator(rd());
+        std::uniform_real_distribution<double> distribution(-0.25, 0.25);
+        
+        for(int i = 0; i < n; i++) {
+            _a.emplace_back(1, params.layers[i].nodes, 0.0);
+
+            if(i < n - 1) {
+                // Add weight matrix (i, i+1)
+                _weights.emplace_back(params.layers[i].nodes, params.layers[i+1].nodes);
+                for(int j = 0; j < params.layers[i].nodes; j++) {
+                    for(int k = 0; k < params.layers[i+1].nodes; k++) {
+                        _weights.back().set_at(j, k, distribution(generator));
+                    }
+                }
+            }
+            if(i > 0) {
+                _z.emplace_back(1, params.layers[i].nodes, 0.0);
+                _biases.emplace_back(1, params.layers[i].nodes, 0.0);
+                _activations.push_back(params.layers[i].activation_function);
+            }
+        }
+
+        _cost = params.cost_function;
+        _learning_rate = params.learning_rate;
+    }
+
+    Matrix Network::apply_cost(Matrix &predicted, Matrix &expected, bool derivative) {
+        int rows = expected.get_rows();
+        int cols = expected.get_cols();
+        Matrix loss(expected.get_rows(), expected.get_cols());
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                loss.set_at(i, j, _cost(predicted.get_at(i, j), expected.get_at(i, j), derivative));
+            }
+        }
+        return loss;
+    }
+
+    Matrix Network::forward(Matrix input) {
+        _a[0] = input;
+
+        int n = _weights.size();
+        for(int i = 0; i < n; i++) {
+            _z[i] = (_a[i] * _weights[i]) + _biases[i];
+            _a[i+1] = _z[i];
+            _a[i+1].map([this, i](double &n) {
+                return _activations[i](n, false);
+            });
+        }
+        return _a.back();
+    }
+
+    void Network::backward(Matrix expected) {
+        Matrix delta_l = apply_cost(_a.back(), expected, true);
+        int n = _activations.size();
+        for(int i = n - 1; i >= 0; i--) {
+            if(i < n - 1) {
+                delta_l *= _weights[i+1].transpose();
+            }
+            _z[i].map([this, i](double &n) {
+                return _activations[i](n, true);
+            });
+            delta_l ^= _z[i];
+
+            // Update weights and biases
+            _weights[i] -= ((_a[i].transpose() * delta_l) * _learning_rate);
+            _biases[i] -= (delta_l * _learning_rate);
+        }
+    }
+
+    void Network::fit(std::vector<double> input, std::vector<double> output) {
+        forward({1, _weights[0].get_rows(), input});
+        backward({1, _z.back().get_cols(), output});
+    }
+}
