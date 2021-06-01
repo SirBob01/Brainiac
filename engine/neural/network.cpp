af8ce7c61f8c74e429a092bc3f1d767b66e8b854
SirBob01 <kleonardo52@gmail.com>
2021-06-01 22:57:43 +1000

    Implement minibatch gradient descent

--- engine/neural/network.cpp
+++ engine/neural/network.cpp
@@ -12,8 +12,10 @@ namespace chess::neural {
             _a.emplace_back(1, params.layers[i].nodes, 0.0);
 
             if(i < n - 1) {
-                // Add weight matrix (i, i+1)
+                // Add weight matrix (i, i+1) and its delta
                 _weights.emplace_back(params.layers[i].nodes, params.layers[i+1].nodes);
+                _weight_deltas.emplace_back(params.layers[i].nodes, params.layers[i+1].nodes);
+
                 for(int j = 0; j < params.layers[i].nodes; j++) {
                     for(int k = 0; k < params.layers[i+1].nodes; k++) {
                         double r = params.random_range * distribution(generator) - (params.random_range / 2);
@@ -23,8 +25,11 @@ namespace chess::neural {
             }
             if(i > 0) {
                 _z.emplace_back(1, params.layers[i].nodes, 0.0);
-                _biases.emplace_back(1, params.layers[i].nodes, 0.0);
                 _activations.push_back(params.layers[i].activation_function);
+            
+                // Add bias matrix and its delta
+                _bias_deltas.emplace_back(1, params.layers[i].nodes, 0.0);
+                _biases.emplace_back(1, params.layers[i].nodes, 0.0);
             }
         }
 
@@ -45,8 +50,8 @@ namespace chess::neural {
         return loss;
     }
 
-    Matrix Network::forward(Matrix input) {
-        _a[0] = input;
+    Matrix Network::forward(std::vector<double> input) {
+        _a[0] = {1, _weights[0].get_rows(), input};
 
         int n = _weights.size();
         for(int i = 0; i < n; i++) {
@@ -59,38 +64,55 @@ namespace chess::neural {
         return _a.back();
     }
 
-    void Network::backward(Matrix expected) {
-        Matrix delta_l = apply_cost(_a.back(), expected, true);
-        int n = _activations.size();
-        for(int i = n - 1; i >= 0; i--) {
-            if(i < n - 1) {
-                delta_l *= _weights[i+1].transpose();
-            }
-            _z[i].map([this, i](double &n) {
-                return _activations[i](n, true);
-            });
-            delta_l ^= _z[i];
+    void Network::fit(std::vector<DataSample> samples, int m) {
+        m = std::min(m, static_cast<int>(samples.size()));
+        double delta_factor = _learning_rate / m;
 
-            Matrix weight_grad = (_a[i].transpose() * delta_l) * _learning_rate;
-            double weight_grad_norm = weight_grad.norm();
-            if(weight_grad_norm >_gradient_clip) {
-                weight_grad *= _gradient_clip / weight_grad_norm;
+        for(int i = 0; i < samples.size(); i++) {
+            DataSample &sample = samples[i];
+            forward(sample.input);
+            
+            Matrix expected = {1, _z.back().get_cols(), sample.output};
+            Matrix delta_l = apply_cost(_a.back(), expected, true);
+            
+            // Backpropagation algorithm
+            int n = _activations.size();
+            for(int j = n - 1; j >= 0; j--) {
+                if(j < n - 1) {
+                    delta_l *= _weights[j+1].transpose();
+                }
+                _z[j].map([this, j](double &n) {
+                    return _activations[j](n, true);
+                });
+                delta_l ^= _z[j];
+
+                _weight_deltas[j] += _a[j].transpose() * delta_l;
+                _bias_deltas[j] += delta_l;
             }
 
-            Matrix bias_grad = delta_l * _learning_rate;
-            double bias_grad_norm = bias_grad.norm();
-            if(bias_grad_norm >_gradient_clip) {
-                bias_grad *= _gradient_clip / bias_grad_norm;
-            }
+            // Update the network parameters in batches
+            if(i % m == 0) {
+                for(int j = 0; j < _activations.size(); j++) {
+                    Matrix weight_grad = _weight_deltas[j] * delta_factor;
+                    double weight_grad_norm = weight_grad.norm();
+                    if(weight_grad_norm >_gradient_clip) {
+                        weight_grad *= _gradient_clip / weight_grad_norm;
+                    }
 
-            // Update weights and biases
-            _weights[i] -= weight_grad;
-            _biases[i] -= bias_grad;
+                    Matrix bias_grad = _bias_deltas[j] * delta_factor;
+                    double bias_grad_norm = bias_grad.norm();
+                    if(bias_grad_norm >_gradient_clip) {
+                        bias_grad *= _gradient_clip / bias_grad_norm;
+                    }
+
+                    _weights[j] -= weight_grad;
+                    _biases[j] -= bias_grad;
+                    
+                    // Reset the deltas
+                    _weight_deltas[j].zero();
+                    _bias_deltas[j].zero();
+                }
+            }
         }
-    }
-
-    void Network::fit(std::vector<double> input, std::vector<double> output) {
-        forward({1, _weights[0].get_rows(), input});
-        backward({1, _z.back().get_cols(), output});
     }
 }
