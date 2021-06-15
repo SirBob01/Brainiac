6f44285beae946eba739904f6862b5ae5ae536a5
SirBob01 <kleonardo52@gmail.com>
2021-06-16 05:41:49 +1000

    Add implementations for core NEAT algorithm

--- engine/neural/random.h
+++ engine/neural/random.h
@@ -2,12 +2,15 @@
 #define CHESS_NEURAL_RANDOM_H_
 
 #include <random>
+#include <chrono>
 
 namespace chess::neural {
     static std::default_random_engine rng;
     static std::uniform_real_distribution<double> uniform(0, 1);
-    
+
     double random();
+
+    void randseed();
 
     int randrange(int start, int stop);
 }
