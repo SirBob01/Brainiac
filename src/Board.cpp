a1dcb2476cccf5e40a6ca9dd164d0456449e240a
Keith Leonardo <kleonardo52@gmail.com>
2024-03-30 03:14:07 +1100

    Refactor Board

--- src/Board.cpp
+++ src/Board.cpp
@@ -10,12 +10,14 @@ namespace Brainiac {
         _pieces[sq] = piece;
     }
 
-    Piece Board::get(uint8_t rank, uint8_t file) const {
-        return get(Square(rank * 8 + file));
-    }
-
-    void Board::set(uint8_t rank, uint8_t file, Piece piece) {
-        return set(Square(rank * 8 + file), piece);
+    void Board::clear(Square sq) {
+        Piece piece = get(sq);
+        if (piece.type() != PieceType::Empty) {
+            Bitboard mask = ~get_square_mask(sq);
+            _bitboards[12 + static_cast<uint8_t>(piece.color())] &= mask;
+            _bitboards[piece.index()] &= mask;
+            _pieces[sq] = Piece();
+        }
     }
 
     void Board::print() const {
@@ -23,7 +25,7 @@ namespace Brainiac {
         for (int rank = 7; rank >= 0; rank--) {
             std::cout << rank + 1 << " ";
             for (int file = 0; file < 8; file++) {
-                Piece piece = get(rank, file);
+                Piece piece = get(Square(rank * 8 + file));
                 if (piece.type() != PieceType::Empty) {
                     std::cout << piece.icon() << " ";
                 } else {
