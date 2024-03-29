fc4024f5760ca9a8108a04005b65186a598f93bf
Keith Leonardo <keith@skand.io>
2024-03-30 01:07:21 +1100

    Implement tokenize utility and unit test

---
+++ src/Utils.cpp
@@ -0,0 +1,18 @@
+#include "Utils.hpp"
+
+namespace Brainiac {
+    std::vector<std::string> tokenize(std::string base, char delimiter) {
+        std::vector<std::string> tokens;
+        std::string current = "";
+        for (auto &c : base) {
+            if (c == delimiter) {
+                if (current.length()) tokens.push_back(current);
+                current = "";
+            } else {
+                current += c;
+            }
+        }
+        if (current.length()) tokens.push_back(current);
+        return tokens;
+    }
+} // namespace Brainiac
---
+++ tests/src/Utils-test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include "../../src/Utils.hpp"
+
+#include "ctest.hpp"
+
+using namespace Brainiac;
+
+int tests_run = 0;
+
+static char *test_tokenize() {
+    std::vector<std::string> tokens;
+    tokens = tokenize("hello, world");
+    mu_assert("'hello, world' tokens",
+              tokens[0] == "hello," && tokens[1] == "world");
+
+    tokens = tokenize("single");
+    mu_assert("'single' tokens", tokens[0] == "single");
+
+    tokens = tokenize("  random  arbitrary   spacing ");
+    mu_assert("'  random  arbitrary   spacing ' tokens",
+              tokens[0] == "random" && tokens[1] == "arbitrary" &&
+                  tokens[2] == "spacing");
+
+    tokens = tokenize("    ");
+    mu_assert("'    ' tokens (empty)", tokens.empty());
+    return 0;
+}
+
+static char *all_tests() {
+    mu_run_test(test_tokenize);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    char *result = all_tests();
+    if (result != 0) {
+        std::cout << "FAILED... " << result << "\n";
+    } else {
+        std::cout << "ALL TESTS PASSED\n";
+    }
+    std::cout << "Number of tests run: " << tests_run << "\n";
+
+    return result != 0;
+}
