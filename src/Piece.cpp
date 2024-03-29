5a764add029c1f35f0ab36db43f65dd2b41cb918
Keith Leonardo <keith@skand.io>
2024-03-30 00:59:18 +1100

    update piece representation

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -1,15 +1,19 @@
 #include "Piece.hpp"
 
 namespace Brainiac {
-    Piece::Piece(PieceType type, Color color) : _bitfield(type << 1 | color){};
+    Piece::Piece(PieceType type, Color color) :
+        _bitfield(static_cast<uint8_t>(type) << 1 |
+                  static_cast<uint8_t>(color)){};
 
-    Piece::Piece() : _bitfield(0){};
+    Piece::Piece() : Piece(PieceType::Empty, Color::Empty){};
 
     PieceType Piece::type() const { return PieceType(_bitfield >> 1); }
 
     Color Piece::color() const { return Color(_bitfield & 1); }
 
-    unsigned Piece::index() const { return color() * 6 + (type() - 1); }
+    unsigned Piece::index() const {
+        return static_cast<uint8_t>(color()) * 6 + static_cast<uint8_t>(type());
+    }
 
     const char Piece::character() const { return PIECE_CHARS[index()]; }
 
