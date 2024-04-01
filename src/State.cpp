bb60b9e92ad3308b38bcfdaf13fde99897d0e2d4
Keith Leonardo <keith@skand.io>
2024-04-02 03:14:07 +1100

    Implement initial infrastructure for Game state

---
+++ src/State.cpp
@@ -0,0 +1,11 @@
+#include "State.hpp"
+
+namespace Brainiac {
+    void State::compute_hash() {
+        // TODO: Algorithm to compute the zobrist hash
+    }
+
+    void State::generate_moves(Color turn) {
+        // TODO: Fast legal move generator
+    }
+} // namespace Brainiac
