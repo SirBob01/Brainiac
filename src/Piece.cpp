c61c91ec91b07880916b26447212d6e9b1bafd9e
Keith Leonardo <keith@skand.io>
2024-04-25 13:14:31 +1000

    Implement get_piece_color

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -4,4 +4,8 @@ namespace Brainiac {
     Piece create_piece(PieceType type, Color color) {
         return static_cast<Piece>(color * 6 + type);
     }
+
+    Color get_piece_color(Piece piece) {
+        return static_cast<Color>(piece >= 6);
+    }
 } // namespace Brainiac
--- tests/src/Piece-test.cpp
+++ tests/src/Piece-test.cpp
@@ -27,9 +27,31 @@ static char *test_create_piece() {
     return 0;
 }
 
+static char *test_get_piece_color() {
+    mu_assert("WhiteQueen", get_piece_color(Piece::WhiteQueen) == Color::White);
+    mu_assert("WhiteBishop",
+              get_piece_color(Piece::WhiteBishop) == Color::White);
+    mu_assert("WhiteRook", get_piece_color(Piece::WhiteRook) == Color::White);
+    mu_assert("WhitePawn", get_piece_color(Piece::WhitePawn) == Color::White);
+    mu_assert("WhiteKing", get_piece_color(Piece::WhiteKing) == Color::White);
+    mu_assert("WhiteKnight",
+              get_piece_color(Piece::WhiteKnight) == Color::White);
+
+    mu_assert("BlackQueen", get_piece_color(Piece::BlackQueen) == Color::Black);
+    mu_assert("BlackBishop",
+              get_piece_color(Piece::BlackBishop) == Color::Black);
+    mu_assert("BlackRook", get_piece_color(Piece::BlackRook) == Color::Black);
+    mu_assert("BlackPawn", get_piece_color(Piece::BlackPawn) == Color::Black);
+    mu_assert("BlackKing", get_piece_color(Piece::BlackKing) == Color::Black);
+    mu_assert("BlackKnight",
+              get_piece_color(Piece::BlackKnight) == Color::Black);
+    return 0;
+}
+
 static char *all_tests() {
     mu_run_test(test_piece_index);
     mu_run_test(test_create_piece);
+    mu_run_test(test_get_piece_color);
     return 0;
 }
 
