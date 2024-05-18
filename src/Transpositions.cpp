76ff50fbd8f42793dbca8a69527f36c1b9aa8e9d
Keith Leonardo <kleonardo52@gmail.com>
2024-05-19 00:40:18 +1000

    Add invalid transposition entry node

--- src/Transpositions.cpp
+++ src/Transpositions.cpp
@@ -10,7 +10,7 @@ namespace Brainiac {
     void Transpositions::set(Position &pos, TableEntry entry) {
         // Overwrite if new entry's depth if higher
         unsigned index = pos.hash() & TABLE_MASK;
-        if (_table[index].value == -INFINITY ||
+        if (_table[index].type == NodeType::Invalid ||
             _table[index].depth >= entry.depth) {
             _table[index] = entry;
         }
