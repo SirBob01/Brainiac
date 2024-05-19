f1ac7984495a5bc413d8640fdd6a2545471b23c4
Keith Leonardo <keith@skand.io>
2024-05-19 13:22:23 +1000

    Implement piece placement evaluation function

--- src/Evaluation.cpp
+++ src/Evaluation.cpp
@@ -1,4 +1,5 @@
 #include "Evaluation.hpp"
+#include "Piece.hpp"
 
 namespace Brainiac {
     short compute_material(const Board &board) {
@@ -7,6 +8,66 @@ namespace Brainiac {
             Piece piece = static_cast<Piece>(i);
             Bitboard bitboard = board.bitboard(piece);
             total += count_set_bitboard(bitboard) * PIECE_WEIGHTS[i];
+        }
+        return total;
+    }
+
+    short compute_placement(const Board &board) {
+        short total = 0;
+        for (uint8_t r = 0; r < 8; r++) {
+            for (uint8_t c = 0; c < 8; c++) {
+                Square sq = static_cast<Square>(r * 8 + c);
+                Piece piece = board.get(sq);
+                Color color = get_piece_color(piece);
+
+                // Invert the board if the piece is black
+                uint8_t index = color == Color::White ? sq : ((7 - r) * 8 + c);
+
+                switch (piece) {
+                // White pieces
+                case Piece::WhitePawn:
+                    total += PAWN_MATRIX[index];
+                    break;
+                case Piece::WhiteKnight:
+                    total += KNIGHT_MATRIX[index];
+                    break;
+                case Piece::WhiteBishop:
+                    total += BISHOP_MATRIX[index];
+                    break;
+                case Piece::WhiteRook:
+                    total += ROOK_MATRIX[index];
+                    break;
+                case Piece::WhiteQueen:
+                    total += QUEEN_MATRIX[index];
+                    break;
+                case Piece::WhiteKing:
+                    total += KING_MATRIX[index];
+                    break;
+
+                // Black pieces
+                case Piece::BlackPawn:
+                    total -= PAWN_MATRIX[index];
+                    break;
+                case Piece::BlackKnight:
+                    total -= KNIGHT_MATRIX[index];
+                    break;
+                case Piece::BlackBishop:
+                    total -= BISHOP_MATRIX[index];
+                    break;
+                case Piece::BlackRook:
+                    total -= ROOK_MATRIX[index];
+                    break;
+                case Piece::BlackQueen:
+                    total -= QUEEN_MATRIX[index];
+                    break;
+                case Piece::BlackKing:
+                    total -= KING_MATRIX[index];
+                    break;
+
+                default:
+                    break;
+                }
+            }
         }
         return total;
     }
@@ -24,6 +85,8 @@ namespace Brainiac {
 
         // Non-leaf node (depth capped)
         const Board &board = pos.board();
-        return sign * compute_material(board);
+        short material = compute_material(board);
+        short placement = compute_placement(board);
+        return sign * (material + placement);
     }
 } // namespace Brainiac
